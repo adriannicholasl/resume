fbddcb28a3b96aa7503cab1ba33c04ca85b007b2
Adrian Nicholas Lumowa <42321588+adriannicholasl@users.noreply.github.com>
2025-06-13 11:39:37 +0800

    Fix CSS Mixed Content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,15 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>@yield('Adrian Nicholas L', 'Portfolio')</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo.ico') }}" type="image/x-icon">
+    <title>@yield('title', 'Adrian Nicholas L | Portfolio')</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ secure_asset('assets/images/logo.ico') }}" type="image/x-icon">
+
+    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
@@ -14,7 +20,8 @@
 <body>
     @yield('content')
 
-    <script src="{{ asset('assets/js/script.js') }}"></script>
+    <!-- JS -->
+    <script src="{{ secure_asset('assets/js/script.js') }}"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
